c36f478245f130924f5e7ebdeaeb2ed18ba8f997
David E. Uribe F <daviduribe998@gmail.com>
2020-12-10 04:44:07 -0300

    Prototipo de vistas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,37 +1,40 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <title>CodePen - Boa Vida</title>
-  <link rel="stylesheet" href="css/login.css">
+    <meta charset="UTF-8">
+    <title>CodePen - Boa Vida</title>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 
 </head>
+
 <body>
-<!-- partial:index.partial.html -->
-<div class="container">
-    <div class="welcome">
-        <div class="pinkbox">
-            <div class="signup nodisplay">
+    <!-- partial:index.partial.html -->
+    <div class="container">
+        <div class="welcome">
+            <div class="pinkbox">
+                <div class="signup nodisplay">
+                </div>
+                <div class="signin">
+                    <h1 class="title"><span>Iniciar sesion</h1>
+                    <hr grey width="75%" />
+                    <form class="more-padding" autocomplete="off">
+                        <input type="text" placeholder="Usuario">
+                        <input type="password" placeholder="Contraseña">
+                        <div class="checkbox">
+                            <input type="checkbox" id="remember" /><label for="remember">Guardar</label>
+                        </div>
+                        <button class="button submit">Ingresar</button>
+                    </form>
+                </div>
             </div>
-            <div class="signin">
-                <h1 class="title"><span>Iniciar sesion</h1>
-                <hr grey width="75%" />
-                <form class="more-padding" autocomplete="off">
-                    <input type="text" placeholder="Usuario">
-                    <input type="password" placeholder="Contraseña">
-                    <div class="checkbox">
-                        <input type="checkbox" id="remember" /><label for="remember">Guardar</label>
-                    </div>
-                    <button class="button submit">Ingresar</button>
-                </form>
+            <div class="rightbox">
+                <img class="flower" src="https://i.ibb.co/sg1Wt1v/31389384-217395775700141-2074199862273900544-o.jpg" alt="" />
             </div>
-        </div>
-        <div class="rightbox">
-            <img class="flower" src="https://i.ibb.co/sg1Wt1v/31389384-217395775700141-2074199862273900544-o.jpg" alt=""/>
         </div>
     </div>
-</div>
-<!-- partial -->
+    <!-- partial -->
 
 </body>
+
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,80 +1,83 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <title>CodePen - Boa Vida</title>
-  <link rel="stylesheet" href="css/home.css">
+    <meta charset="UTF-8">
+    <title>CodePen - Boa Vida</title>
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
 
 </head>
+
 <body>
-<!-- partial:index.partial.html -->
-<nav>
-    <a id="resp-menu" class="responsive-menu" href="#"><i class="fa fa-reorder"></i> Menu</a>
-    <ul class="menu">
+    <!-- partial:index.partial.html -->
+    <nav>
+        <a id="resp-menu" class="responsive-menu" href="#"><i class="fa fa-reorder"></i> Menu</a>
+        <ul class="menu">
 
-        <li><a href="#"><i class="fa fa-user"></i> Boa Vida</a></li>
+            <li><a href="#"><i class="fa fa-user"></i> Boa Vida</a></li>
 
-        <li><a href="#"><i class="fa fa-camera"></i> Productos</a>
-            <ul class="sub-menu">
-                <li><a href="#">Stock</a></li>
-                <li><a href="#">Sub-Menu 2</a>
-                    <ul>
-                        <li><a href="#">Sub Sub-Menu 1</a></li>
-                        <li><a href="#">Sub Sub-Menu 2</a></li>
-                        <li><a href="#">Sub Sub-Menu 3</a></li>
-                        <li><a href="#">Sub Sub-Menu 4</a></li>
-                        <li><a href="#">Sub Sub-Menu 5</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Sub-Menu 3</a>
-                    <ul>
-                        <li><a href="#">Sub Sub-Menu 1</a></li>
-                        <li><a href="#">Sub Sub-Menu 2</a></li>
-                        <li><a href="#">Sub Sub-Menu 3</a></li>
-                        <li><a href="#">Sub Sub-Menu 4</a></li>
-                        <li><a href="#">Sub Sub-Menu 5</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-        <li><a href="#"><i class="fa fa-bullhorn"></i> Ventas</a></li>
-        <li><a href="#"><i class="fa fa-tags"></i> Usuarios</a>
-            <ul class="sub-menu">
-                <li><a href="#">Registrar usuario</a></li>
-                <li><a href="#">Sub-Menu 2</a>
-                    <ul>
-                        <li><a href="#">Sub Sub-Menu 1</a></li>
-                        <li><a href="#">Sub Sub-Menu 2</a></li>
-                        <li><a href="#">Sub Sub-Menu 3</a></li>
-                        <li><a href="#">Sub Sub-Menu 4</a></li>
-                        <li><a href="#">Sub Sub-Menu 5</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Sub-Menu 3</a>
-                    <ul>
-                        <li><a href="#">Sub Sub-Menu 1</a></li>
-                        <li><a href="#">Sub Sub-Menu 2</a></li>
-                        <li><a href="#">Sub Sub-Menu 3</a></li>
-                        <li><a href="#">Sub Sub-Menu 4</a></li>
-                        <li><a href="#">Sub Sub-Menu 5</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </li>
-    </ul>
-</nav>
-<div class="welcome">
-    <div class="container">
-        <div class="flower" <a href="https://ibb.co/sg1Wt1v"><img src="https://i.ibb.co/j8wJHwv/31389384-217395775700141-2074199862273900544-o.jpg" alt="31389384-217395775700141-2074199862273900544-o">
+            <li><a href="#"><i class="fa fa-camera"></i> Productos</a>
+                <ul class="sub-menu">
+                    <li><a href="#">Stock</a></li>
+                    <li><a href="#">Sub-Menu 2</a>
+                        <ul>
+                            <li><a href="#">Sub Sub-Menu 1</a></li>
+                            <li><a href="#">Sub Sub-Menu 2</a></li>
+                            <li><a href="#">Sub Sub-Menu 3</a></li>
+                            <li><a href="#">Sub Sub-Menu 4</a></li>
+                            <li><a href="#">Sub Sub-Menu 5</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Sub-Menu 3</a>
+                        <ul>
+                            <li><a href="#">Sub Sub-Menu 1</a></li>
+                            <li><a href="#">Sub Sub-Menu 2</a></li>
+                            <li><a href="#">Sub Sub-Menu 3</a></li>
+                            <li><a href="#">Sub Sub-Menu 4</a></li>
+                            <li><a href="#">Sub Sub-Menu 5</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+            <li><a href="#"><i class="fa fa-bullhorn"></i> Ventas</a></li>
+            <li><a href="#"><i class="fa fa-tags"></i> Usuarios</a>
+                <ul class="sub-menu">
+                    <li><a href="#">Registrar usuario</a></li>
+                    <li><a href="#">Sub-Menu 2</a>
+                        <ul>
+                            <li><a href="#">Sub Sub-Menu 1</a></li>
+                            <li><a href="#">Sub Sub-Menu 2</a></li>
+                            <li><a href="#">Sub Sub-Menu 3</a></li>
+                            <li><a href="#">Sub Sub-Menu 4</a></li>
+                            <li><a href="#">Sub Sub-Menu 5</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Sub-Menu 3</a>
+                        <ul>
+                            <li><a href="#">Sub Sub-Menu 1</a></li>
+                            <li><a href="#">Sub Sub-Menu 2</a></li>
+                            <li><a href="#">Sub Sub-Menu 3</a></li>
+                            <li><a href="#">Sub Sub-Menu 4</a></li>
+                            <li><a href="#">Sub Sub-Menu 5</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <div class="welcome">
+        <div class="container">
+            <div class="flower" <a href="https://ibb.co/sg1Wt1v"><img src="https://i.ibb.co/j8wJHwv/31389384-217395775700141-2074199862273900544-o.jpg" alt="31389384-217395775700141-2074199862273900544-o">
 
-        </a>
+                </a>
 
-        <h1>Bienvenido:</h1>
-        <h2>"Usuario"</h2>
-        <p>Al sistema de inventario "Boa Vida"</p>
+                <h1>Bienvenido:</h1>
+                <h2>"Usuario"</h2>
+                <p>Al sistema de inventario "Boa Vida"</p>
 
-    </div>
-</div>
+            </div>
+        </div>
 
 </body>
+
 </html>
