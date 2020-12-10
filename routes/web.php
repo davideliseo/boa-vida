421c5013d75ee32909ab1e371ceeeae20db6147c
David E. Uribe F <daviduribe998@gmail.com>
2020-12-10 05:10:46 -0300

    Rutas de las entidades

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductosController;
+use App\Http\Controllers\InsumosController;
+use App\Http\Controllers\VentasController;
+use App\Http\Controllers\ProveedoresController;
+use App\Http\Controllers\UsuariosController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +22,31 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/inicio', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/inicio', [HomeController::class, 'index'])->name('home');
 
 Route::redirect('/', '/inicio');
+
+Route::get('/productos',                   [ProductosController::class, 'index'])->name('productos');
+Route::get('/productos/{producto}',        [ProductosController::class, 'show'])->name('productos.show');
+Route::get('/productos/agregar',           [ProductosController::class, 'create'])->name('productos.create');
+Route::get('/productos/{producto}/editar', [ProductosController::class, 'update'])->name('productos.update');
+
+Route::get('/insumos',                 [InsumosController::class, 'index'])->name('insumos');
+Route::get('/insumos/{insumo}',        [InsumosController::class, 'show'])->name('insumos.show');
+Route::get('/insumos/agregar',         [InsumosController::class, 'create'])->name('insumos.create');
+Route::get('/insumos/{insumo}/editar', [InsumosController::class, 'update'])->name('insumos.update');
+
+Route::get('/ventas',                [VentasController::class, 'index'])->name('ventas');
+Route::get('/ventas/{venta}',        [VentasController::class, 'show'])->name('ventas.show');
+Route::get('/ventas/agregar',        [VentasController::class, 'create'])->name('ventas.create');
+Route::get('/ventas/{venta}/editar', [VentasController::class, 'update'])->name('ventas.update');
+
+Route::get('/proveedores',                    [ProveedoresController::class, 'index'])->name('proveedores');
+Route::get('/proveedores/{proveedor}',        [ProveedoresController::class, 'show'])->name('proveedores.show');
+Route::get('/proveedores/agregar',            [ProveedoresController::class, 'create'])->name('proveedores.create');
+Route::get('/proveedores/{proveedor}/editar', [ProveedoresController::class, 'update'])->name('proveedores.update');
+
+Route::get('/usuarios',                  [UsuariosController::class, 'index'])->name('usuarios');
+Route::get('/usuarios/{usuario}',        [UsuariosController::class, 'show'])->name('usuarios.show');
+Route::get('/usuarios/agregar',          [UsuariosController::class, 'create'])->name('usuarios.create');
+Route::get('/usuarios/{usuario}/editar', [UsuariosController::class, 'update'])->name('usuarios.update');
