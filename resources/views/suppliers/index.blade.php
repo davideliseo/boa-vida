c690060948b2bb159a160416024d95fc4d4dcca9
David E. Uribe F <david.uribef@utem.cl>
2020-12-28 06:03:08 -0300

    Refactorización vistas de proveedores

--- resources/views/suppliers/index.blade.php
+++ resources/views/suppliers/index.blade.php
@@ -1,29 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-card.index resource="suppliers" :collection="$collection">
-        <x-slot name="table">
-            <x-slot name="headers">
-                    <x-table.header resource="suppliers" key="name" />
-                    <x-table.header resource="suppliers" key="rut" />
-                    <x-table.header resource="suppliers" key="email" />
-                    <x-table.header resource="suppliers" key="address" />
-                    <x-table.header resource="suppliers" key="phone_number" />
-            </x-slot>
-            <x-slot name="rows">
-                @foreach ($collection as $supplier)
-                    <tr>
-                        <x-table.cell resource="suppliers" key="name"         :item="$supplier" />
-                        <x-table.cell resource="suppliers" key="rut"          :item="$supplier" />
-                        <x-table.cell resource="suppliers" key="email"        :item="$supplier" />
-                        <x-table.cell resource="suppliers" key="address"      :item="$supplier" />
-                        <x-table.cell resource="suppliers" key="phone_number" :item="$supplier" />
-                        <td>
-                            <x-table.row.actions resource="suppliers" :item="$supplier" />
-                        </td>
-                    </tr>
-                @endforeach
-            </x-slot>
-        </x-slot>
-    </x-card.index>
+    <x-resource.index resource="suppliers" :collection="$collection" />
 @endsection
--- resources/views/suppliers/show.blade.php
+++ resources/views/suppliers/show.blade.php
@@ -1,21 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-card.show title="Ficha de proveedor" resource="suppliers" :item="$supplier">
-        <x-slot name="fields">
-            <x-form.row.show resource="suppliers" key="id">
-                <x-slot name="value">
-                    {{ '#' . $supplier->id }}
-                </x-slot>
-            </x-form.row.show>
-
-            <x-form.row.show resource="suppliers" key="name"         :item="$supplier" />
-            <x-form.row.show resource="suppliers" key="rut"          :item="$supplier" />
-            <x-form.row.show resource="suppliers" key="email"        :item="$supplier" />
-            <x-form.row.show resource="suppliers" key="address"      :item="$supplier" />
-            <x-form.row.show resource="suppliers" key="phone_number" :item="$supplier" />
-            <x-form.row.show resource="suppliers" key="created_at"   :item="$supplier" />
-            <x-form.row.show resource="suppliers" key="updated_at"   :item="$supplier" />
-        </x-slot>
-    </x-card.show>
+    <x-resource.show resource="suppliers" title="Ficha de proveedor" :item="$supplier" />
 @endsection
