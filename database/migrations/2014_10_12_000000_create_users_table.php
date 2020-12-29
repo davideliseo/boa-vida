62c49369d225f5f1a94f7494fb270c1070fc9fc2
David E. Uribe F <david.uribef@utem.cl>
2020-12-28 21:54:37 -0300

    Atributo de administrador en usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,17 +16,15 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('rut')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
 
-            // TODO: arreglar
-            $table->string('rut')->unique()->nullable();
             $table->string('address')->nullable();
             $table->string('phone_number')->nullable();
+            $table->boolean('admin')->default(false);
 
-            // TODO: mover timestamps al final.
             $table->timestamps();
         });
     }
